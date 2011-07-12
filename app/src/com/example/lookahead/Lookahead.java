1c64666273a5481b6329f6ffdfc8d7c577c9443b
zero10 <ctrowat@gmail.com>
2011-07-12 12:33:25 -0600

    committing a hello world dummy project

---
+++ app/src/com/example/lookahead/Lookahead.java
@@ -0,0 +1,14 @@
+package com.example.lookahead;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Lookahead extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
