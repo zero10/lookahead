28a09a1cbbfcb37cbc75890def1d4eab375bb1a6
zero10 <ctrowat@gmail.com>
2011-07-14 12:53:03 -0600

    Some random mucking about

--- app/src/com/example/lookahead/Lookahead.java
+++ app/src/com/example/lookahead/Lookahead.java
@@ -2,6 +2,8 @@ package com.example.lookahead;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Lookahead extends Activity {
@@ -10,5 +12,17 @@ public class Lookahead extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void selfDestruct(View view)
+    {
+    	setContentView(R.layout.log);
+	}
+    
+    public void addLog(View view)
+    {
+    	TextView logView = (TextView) findViewById(R.id.logText);
+    	logView.setText(R.string.gibberish);
+    	
     }
 }
